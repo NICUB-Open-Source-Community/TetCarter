59445ae65e573c187f4334c58f5ddd3ce4b65553
Godzuche <godxuche@gmail.com>
2023-07-01 16:19:31 +0100

    created the onboarding screen ui

--- app/src/main/java/com/example/helcare/core/ui/HelCareApp.kt
+++ app/src/main/java/com/example/helcare/core/ui/HelCareApp.kt
@@ -1,7 +1,9 @@
 package com.example.helcare.core.ui
 
 import androidx.compose.runtime.Composable
+import com.example.helcare.feature.onboarding.OnboardingRoute
 
 @Composable
 fun HelCareApp() {
+    OnboardingRoute()
 }
